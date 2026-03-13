d92843dd52df84b0fd8f5371a8b5c955f78d0836
Veehansystum <145607257+Veehansystum@users.noreply.github.com>
2026-03-13 18:08:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,11 @@ const cors =require('cors');
  app.get('/',(req,res)=>{
     res.send('Hello from the server ..');
 })
-app.use(cors())
+app.use(cors({
+ origin:"*",
+ methods:['GET','POST','PUT','DELETE'],
+ allowedHeaders:['Content-Type]
+}));
 app.use(bodyParser.json());
 app.use('/tasks',TaskRouter)
 app.listen(PORT, () => {
